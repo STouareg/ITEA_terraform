779ba98d5f8be34a6fa4c81999b02bef3fd20814
s.turyk <sergiituryk@gmail.com>
2021-02-08 16:02:21 +0200

    Init

---
+++ network.tf
@@ -0,0 +1,79 @@
+resource "aws_vpc" "lesson7-vpc" {
+    cidr_block = "172.31.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    enable_classiclink = false
+    instance_tenancy = "default" 
+    
+    tags = {
+      "Name" = "lesson7-vpc"
+    }
+}
+
+resource "aws_vpc" "lesson7-subnet-public-1" {
+    vpc_id = aws_vpc.lesson7-vpc.id
+    cird_block = "172.31.0.0/20"
+    map_public_ip_on_launch = true
+    availability_zone = "eu-central-1a"
+    tags = {
+        "Name" = "lesson7-subnet-public-1"
+    }
+}
+
+resource "aws_internet_gateway" "lesson7-igw" {
+    vpc_id = aws_vpc.lesson7-vpc.id
+    tags = {
+        "Name" = "lesson7-igw"
+    }
+}
+
+resource "aws_route_table" "lesson7-public-crt" {
+    vpc_id = aws_vpc.lesson7-vpc.id
+    route {
+        cird_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.lesson7-igw.id
+    }
+}
+
+resource "aws_route_table_association" "lesson7-crta-public-subnet-1"{
+    subnet_id = aws_subnet.lesson7-subnet-public-1.id
+    route_table_id = aws_route_table.lesson7-public-crt.id
+}
+
+resource "aws_security_group" "lesson7-http-rdp-winrm"{
+    vpc_id = aws_vpc.lesson7-vpc.id
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_block = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cird_block = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 3389
+        to_port = 3389
+        protocol = "tcp"
+        cidr_block = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 5985
+        to_port = 5985
+        protocol = "tcp"
+        cidr_block = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 5986
+        to_port = 5986
+        protocol = "tcp"
+        cidr_block = ["0.0.0.0/0"]
+    }
+    tags = {
+        "Name" = "lesson7-http-rdp-winrm"
+    }
+}
